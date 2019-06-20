127b42e3c17b1f915a3d4801f6aec42441088318
ilayWW <ilay@wnwd.com>
2019-06-20 09:18:04 +0300

    Adding vessel duration in port component

--- client/src/components/GaugeGroup.component.js
+++ client/src/components/GaugeGroup.component.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Chart from "react-apexcharts";
 import { inject, observer } from 'mobx-react';
 import Grid from "@material-ui/core/Grid";
+import AccessTimeOutlinedIcon from "@material-ui/icons/AccessTimeOutlined";
+import Typography from "@material-ui/core/Typography";
 
 @inject('routing')
 @inject('portsStore')
@@ -16,9 +18,9 @@ class GaugeGroup extends React.Component {
             .then(res => res.json());
         const [portcallsUnique] = await fetch(`http://localhost:9000/ports/portcallsUnique/${ this.props.id }`)
             .then(res => res.json());
-
         const uniqueRatio = (100 * (portcallsUnique.portcalls_uniq / portcalls.portcalls_last_year)).toFixed(2);
         this.setState({
+            durations,
             portcallsRatioChart: {
                 options: {
                     title: {
@@ -55,12 +57,24 @@ class GaugeGroup extends React.Component {
                     </div>
                 </Grid>
                 <Grid item xs>
-                    <div style={ { height: '100%' } }>
-                        <Chart options={ this.state.portcallsRatioChart.options }
-                               series={ this.state.portcallsRatioChart.series }
-                               type="radialBar"/>
-                    </div>
+                        <Grid container justify={'center'} alignItems={'center'} style={ { height: '100%' } }>
+                            <Grid item container direction={'column'} justify={'center'} alignItems={'center'}>
+                                <Grid item>
+                                    <Typography style={{paddingBottom: '10px'}}>
+                                        Average Vessel Duration In Port
+                                    </Typography>
+                                </Grid>
+                                <Grid item style={{fontSize: '80px', color: '#4286f4'}}>
+                                    <AccessTimeOutlinedIcon fontSize={'inherit'}/>
+                                </Grid>
+                                <Grid item>
+                                    <Typography variant={'body1'}>
+                                        {this.state.durations.average_portcall_duration.toFixed(2)} Hours
+                                    </Typography>
 
+                                </Grid>
+                            </Grid>
+                        </Grid>
                 </Grid>
             </Grid>
             }
